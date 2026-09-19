14ebf5830b8afd33b46ac4bfe14ac9d92e1c1096
zizo-26 <abdelazizbon24@gmail.com>
2026-09-19 16:51:43 +0200

    setting up tailwindcss and hot toster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import Homepage from "./Pages/Homepage";
 import CreatePage from "./Pages/CreatePage";
 import NoteDetailsPage from "./Pages/NoteDetailsPage";
+import toast from "react-hot-toast";
 
  function App(){
 
+  // testing tailwindcss
   return(<div>
 
-
+        <button onClick={()=> toast.error('congarts')}  className="btn btn-link"  >click me</button>
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/create" element={<CreatePage />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from 'react-router'
+import {Toaster} from 'react-hot-toast'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <App />
+    <Toaster position='top-center' reverseOrder={false}/>
     </BrowserRouter>
   </StrictMode>,
 )
