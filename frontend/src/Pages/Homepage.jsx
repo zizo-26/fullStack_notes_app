c7611491212cb2032019d2c905d2f1aea8f93ca1
zizo-26 <abdelazizbon24@gmail.com>
2026-09-22 13:26:11 +0200

    importing axios

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,9 +1,34 @@
 // creating a simple homepage for test
+import axios from "axios";
 import Navbar from "../Components/Navbar";
 import RateLimtedUI from "../Components/RateLimtedUI";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function Homepage() {
   const [isRatedLimted, setIsRatedLimited] = useState(false); // Set to true for testing purposes
+  const [Notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+ useEffect(()=> {
+          
+  const fatchNotes= async ()=>{
+
+    try {
+      const res=  await axios.get("http://localhost:5000/api/notes");
+      console.log(res.data)
+      
+    } catch (error) {
+       
+      console.log("error fatching notes" ,error)
+
+    }
+
+  }
+
+  fatchNotes();
+
+ })
+
   return (
     <div className="min-h-screen bg-neutral-950">
       <Navbar />
